262d4bf6530bbbe96dc552b696a2b139002ec97a
raspe <raspe@FRAWSTPROOF2024>
2026-05-05 00:44:43 -0400

    Add project files.

---
+++ Controllers/BudgetController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BudgetController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Ping()
+        {
+            return Ok("Budget API is running. Current DateTime: " + DateTime.Now.ToString());
+        }
+    }
+}
